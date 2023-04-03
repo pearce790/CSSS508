91671bc2713dcf8fa16ae0dc5e76e54fab498675
Michael Pearce <pearce790@gmail.com>
2023-04-03 12:39:33 -0700

    updated lecture 2 materials

--- Lectures/Lecture2/CSSS508_Lecture2_ggplot2.R
+++ Lectures/Lecture2/CSSS508_Lecture2_ggplot2.R
@@ -1,43 +1,33 @@
-# install.packages("gapminder")
-
+# install.packages("gapminder") 
 library(gapminder)
 
 getwd()
 
-# setwd("INSERT YOUR WORKING DIRECTORY NAME HERE")
+# setwd("[YOUR WORKING DIRECTORY]")
 
-new.object <- 1:10 # Making vector of 1 to 10 
-# save(new.object, file="new_object.RData")
-# load("new_object.RData")
 
 str(gapminder)
 
-# install.packages("tidyverse")
+China <- subset(gapminder,
+                gapminder$country == "China")
+plot(lifeExp ~ year, 
+     data = China, 
+     xlab = "Year", 
+     ylab = "Life expectancy",
+     main = "Life expectancy in China", 
+     col = "red", 
+     pch = 16)
 
-library(dplyr)
-
-log(mean(gapminder$pop))
-
-gapminder$pop %>% mean() %>% log()
-
-gapminder %>% filter(country == "Algeria")
-
-head(gapminder$country == "Algeria", 50) # display first 50 elements
-
-gapminder %>%
-  filter(country == "Oman" & year > 1980)
-
-gapminder %>% 
-  filter(country == "Oman" &
-           year > 1980)
-gapminder %>%
-  filter(country == "Oman" |
-           year > 1980)
-
-China <- gapminder %>% filter(country == "China")
-head(China, 4)
-
+# install.packages("ggplot2")
 library(ggplot2)
+
+ggplot(data = China, 
+       aes(x = year, y = lifeExp)) +
+  geom_point(color = "red", size = 3) +
+  xlab("Year") + 
+  ylab("Life expectancy") +
+  ggtitle("Life expectancy in China") +
+  theme_bw(base_size=18)
 
 ggplot(data = China,  #<<
        aes(x = year, y = lifeExp)) #<<
@@ -54,6 +44,11 @@ ggplot(data = China,
        aes(x = year, y = lifeExp)) +
   geom_point(color = "red", size = 3) +
   xlab("Year") #<<
+
+ggplot(data = China, 
+       aes(x = year, y = lifeExp)) +
+  geom_point(color = "red", size = 3) +
+  xlab("Year") 
 
 ggplot(data = China, 
        aes(x = year, y = lifeExp)) +
@@ -83,7 +78,6 @@ ggplot(data = China,
   ylab("Life expectancy") +
   ggtitle("Life expectancy in China") +
   theme_bw(base_size=18) #<<
-
 
 ggplot(data = gapminder,#<<
        aes(x = year, y = lifeExp)) +
@@ -181,13 +175,12 @@ lifeExp_by_year
 lifeExp_by_year +
   theme(legend.position = "bottom")
 
-# ggsave("I_saved_a_file.pdf", plot = lifeExp_by_year,
-#        height = 3, width = 5, units = "in")
+ggsave("I_saved_a_file.pdf", plot = lifeExp_by_year,
+       height = 3, width = 5, units = "in")
 
 ggplot(data = China, aes(x = year, y = gdpPercap)) +
   geom_line() +
-  scale_y_log10(breaks = c(1000, 2000, 3000, 4000, 5000), #<<
-                labels = scales::dollar) + #<<
+  scale_y_log10(breaks = c(1000, 2000, 3000, 4000, 5000)) + #<<
   xlim(1940, 2010) + ggtitle("Chinese GDP per capita")
 
 ggplot(data = China, aes(x = year, y = lifeExp)) +
@@ -196,7 +189,7 @@ ggplot(data = China, aes(x = year, y = lifeExp)) +
   theme_gray(base_size = 20) #<<
 
 lifeExp_by_year +
-  theme(legend.position = c(0.8, 0.2))
+  theme(legend.position = c(0.8, 0.2)) 
 
 lifeExp_by_year +
   theme(legend.position = c(0.8, 0.2)) +
@@ -204,75 +197,6 @@ lifeExp_by_year +
     name = "Which continent are\nwe looking at?", # \n adds a line break #<<
     values = c("Africa" = "seagreen", "Americas" = "turquoise1", 
                "Asia" = "royalblue", "Europe" = "violetred1", "Oceania" = "yellow"))
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line() 
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line() +
-  geom_line(stat = "smooth", method = "loess", #<<
-            aes(group = continent)) #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line() +
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent)) +
-  facet_wrap(~ continent, nrow = 2) #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line(aes(color = "Country")) + #<<
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent, color = "Continent")) + #<<
-  facet_wrap(~ continent, nrow = 2) +
-  scale_color_manual(name = "Life Exp. for:", values = c("Country" = "black", "Continent" = "blue")) #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line(aes(color = "Country", size = "Country")) + #<<
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent, color = "Continent", size = "Continent")) + #<<
-  facet_wrap(~ continent, nrow = 2) +
-  scale_color_manual(name = "Life Exp. for:", values = c("Country" = "black", "Continent" = "blue")) +
-  scale_size_manual(name = "Life Exp. for:", values = c("Country" = 0.25, "Continent" = 3)) #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line(alpha = 0.5, aes(color = "Country", size = "Country")) + #<<
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent, color = "Continent", size = "Continent"), alpha = 0.5) + #<<
-  facet_wrap(~ continent, nrow = 2) +
-  scale_color_manual(name = "Life Exp. for:", values = c("Country" = "black", "Continent" = "blue")) +
-  scale_size_manual(name = "Life Exp. for:", values = c("Country" = 0.25, "Continent" = 3))
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line(alpha = 0.5, aes(color = "Country", size = "Country")) +
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent, color = "Continent", size = "Continent"), alpha = 0.5) +
-  facet_wrap(~ continent, nrow = 2) +
-  scale_color_manual(name = "Life Exp. for:", values = c("Country" = "black", "Continent" = "blue")) +
-  scale_size_manual(name = "Life Exp. for:", values = c("Country" = 0.25, "Continent" = 3)) +
-  theme_minimal(base_size = 14) + ylab("Years") + xlab("") #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line(alpha = 0.5, aes(color = "Country", size = "Country")) +
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent, color = "Continent", size = "Continent"), alpha = 0.5) +
-  facet_wrap(~ continent, nrow = 2) +
-  scale_color_manual(name = "Life Exp. for:", values = c("Country" = "black", "Continent" = "blue")) +
-  scale_size_manual(name = "Life Exp. for:", values = c("Country" = 0.25, "Continent" = 3)) +
-  theme_minimal(base_size = 14) + ylab("Years") + xlab("") + 
-  ggtitle("Life Expectancy, 1952-2007", subtitle = "By continent and country") #<<
-
-ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
-  geom_line(alpha = 0.5, aes(color = "Country", size = "Country")) +
-  geom_line(stat = "smooth", method = "loess", 
-            aes(group = continent, color = "Continent", size = "Continent"), alpha = 0.5) +
-  facet_wrap(~ continent, nrow = 2) +
-  scale_color_manual(name = "Life Exp. for:", values = c("Country" = "black", "Continent" = "blue")) +
-  scale_size_manual(name = "Life Exp. for:", values = c("Country" = 0.25, "Continent" = 3)) +
-  theme_minimal(base_size = 14) + ylab("Years") + xlab("") + 
-  ggtitle("Life Expectancy, 1952-2007", subtitle = "By continent and country") +
-  theme(axis.text.x = element_text(angle = 45)) #<<
 
 ggplot(data = gapminder, aes(x = year, y = lifeExp, group = country)) +
   geom_line(alpha = 0.5, aes(color = "Country", size = "Country")) +
