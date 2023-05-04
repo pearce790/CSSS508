e95cafcb83428681a18ecc4fc2a38452cf5acd2c
Michael Pearce <pearce790@gmail.com>
2023-05-04 09:22:52 -0700

    updating lecture 7 information

--- Lectures/Lecture7/CSSS508_Lecture7_functions.R
+++ Lectures/Lecture7/CSSS508_Lecture7_functions.R
@@ -3,7 +3,6 @@
 #   return(OUTPUT)
 # }
 
-
 double_x <- function(x){
   double_x <- x * 2
   return(double_x)
@@ -14,12 +13,13 @@ double_x(NA)
 double_x(1:2)
 
 first_and_last <- function(x) {
-    first <- x[1]
-    last  <- x[length(x)]
-    return(c("first" = first, "last" = last))
+  first <- x[1]
+  last  <- x[length(x)]
+  return(c("first" = first, "last" = last))
 }
 
 first_and_last(c(4, 3, 1, 8))
+
 first_and_last(7)
 first_and_last(numeric(0))
 
@@ -39,12 +39,11 @@ smarter_first_and_last(c(4, 3, 1, 8))
 smarter_first_and_last
 
 
-for(col_index in seq_along(swiss)){
+for(col_index in 1:ncol(swiss)){
   mean_swiss_col <- mean(swiss[,col_index])
   names_swiss_col <- names(swiss)[col_index]
   print(c(names_swiss_col,round(mean_swiss_col,3)))
 }
-
 
 # apply(DATA, MARGIN, FUNCTION)
 
@@ -55,14 +54,7 @@ head(row_max,20)
 
 apply(swiss,2,summary) # summary of each column
 
-divers_score <- function(x){
-  if(length(x) != 7){
-    stop("x is not of length 7!")
-  } else{
-    x_nofirst_nolast <- x[2:6]
-    return(mean(x_nofirst_nolast))
-  }
-}
 scores <- matrix(1:21,nrow=3)
 print(scores)
-apply(scores,1,divers_score)
+my_function <- function(x){ mean(x+10,na.rm=T) }
+apply(scores,1,my_function)
