7107c5b5b4e3b0319ed6597d1ca03cf16ed7d148
Michael Pearce <pearce790@gmail.com>
2023-05-11 12:54:17 -0700

    updated lecture 8

--- Lectures/Lecture8/CSSS508_Lecture8_strings.R
+++ Lectures/Lecture8/CSSS508_Lecture8_strings.R
@@ -1,11 +1,14 @@
+set.seed(7)
+
+#### Section 1 ####
+
 "01" == "1"
 "01" == 1
 "1" == 1
-
 c(class("1"),class(1))
 
+#### Section 2 ####
 load(url("https://pearce790.github.io/CSSS508/Lectures/Lecture8/restaurants.Rdata"))
-
 names(restaurants)
 dim(restaurants)
 
@@ -17,8 +20,7 @@ table(length_zip)
 substr("98126",3,5)
 
 library(dplyr)
-restaurants <- restaurants %>%
-  mutate(ZIP_5 = substr(Zip_Code, 1, 5))
+restaurants$ZIP_5 <- substr(restaurants$Zip_Code,1,5)
 restaurants %>% distinct(ZIP_5) %>% head()
 
 paste("Michael","Pearce")
@@ -27,11 +29,13 @@ paste("CSSS","508",sep= "_")
 paste(c("CSSS","STAT"),"508",sep= "_")
 paste(c("CSSS","STAT"),"508",sep= "_",collapse=" , ")
 
-restaurants <- restaurants %>%
-  mutate(mailing_address = 
-           paste(Address, ", ", City, ", WA ", ZIP_5, sep = ""))
+restaurants$mailing_address <- 
+  paste(restaurants$Address,", ",
+        restaurants$City,", WA ",restaurants$ZIP_5,
+        sep = "")
 restaurants %>% distinct(mailing_address) %>% head()
 
+#### Section 3 ####
 library(stringr)
 
 nchar("weasels")
@@ -65,11 +69,6 @@ str_detect(string = c("Hello","my name","is Michael"),
            pattern = "m")
 str_detect(string = c("Hello","my name","is Michael"),
            pattern = "M")
-
-str_detect(string = c("Hello","my name","is Michael"),
-           pattern = "m|M")
-str_detect(string = c("Hello","my name","is Michael"),
-           pattern = "m&M")
 
 unique_phones <- unique(restaurants$Phone)
 unique_phones %>% tail(4)
